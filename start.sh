bc663da32fd91b323ed5477c0836df1eef466ba7
Srimadhav2007 <phanisrimadhav.mula@gmail.com>
2025-10-26 09:23:30 +0530

    Changed start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@
 set -e
 
 # Start the FastAPI server in the background
-uvicorn main:app --host 0.0.0.0 --port 8000 &
+uvicorn backend/main:app --host 0.0.0.0 --port 8000 &
 
 # Start the Streamlit app
 # Note: Render's health check will ping the service at the port specified by the PORT env var.
 # Streamlit must run on this port.
-streamlit run app.py --server.port $PORT --server.address 0.0.0.0 --server.enableCORS false
+streamlit run ui/app.py --server.port $PORT --server.address 0.0.0.0 --server.enableCORS false
